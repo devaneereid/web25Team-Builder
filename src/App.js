008037b7815086a39f9a260ead8fa70a7b0e6e93
Devanee Reid <devaneereid@gmail.com>
2019-11-13 17:52:39 -0700

    still working on Form section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React, {useState} from 'react';
 import TeamMembers from "./TeamMembers";
 import TeamInfo from "./TeamInfo";
+import Form from "./Form";
 import './App.css';
 
 function App() {
 
   const [info, setInfo] = useState(TeamInfo);
 
-const handleChanges = e => {
-  setInfo({...info, [e.target.name]: e.target.value});
-  console.log(e.target.value);
-}
+// const handleChanges = e => {
+//   setInfo({...info, [e.target.name]: e.target.value});
+//   console.log(e.target.value);
+// }
   return (
     <div className="App">
       <h1>Team Builder App!</h1>
--- src/Form.js
+++ src/Form.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 const Form = props => {
@@ -9,5 +9,32 @@ const Form = props => {
         role: ''
     });
 
+    const newChanges = e => {
+        setTeamData({...teamData, [e.target.name]: e.target.value});
+        console.log(e.target.value);
+      } 
+    
+    const submitForm = e => {
+        e.preventDefault();
+        props.addTeamData(teamData);
+        setTeamData({
+            name: '',
+            email: '',
+            role: ''
+        });
+
+    return (
+        <form onSubmit = {submitForm}>
+            {/* <label htmlFor="title">Title</label> */}
+            <input 
+                name="name"
+                type="text"
+                onChange={newChanges}
+                value={teamData.body}
+            />
+
+        </form>
+    )
+    }
 }
 export default Form;
